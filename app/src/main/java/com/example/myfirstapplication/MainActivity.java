bdbc8dac79c14b34723c889141d1e2776b210d48
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-01-01 18:47:13 +0900

    メンバー変数とレイアウト部品を結びつけた

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,17 +3,37 @@ package com.example.myfirstapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton radioGoo;
+    private RadioButton radioChoki;
+    private RadioButton radioPaa;
+
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGoo = (RadioButton)findViewById(R.id.radioGoo);
+        radioChoki = (RadioButton)findViewById(R.id.radioChoki);
+        radioPaa = (RadioButton)findViewById(R.id.radioPaa);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
     }
 
     public void onDuel(View view) {
+        String result = "残念でした";
+        if (radioGoo.isChecked()) {
+            result = "引き分けでした";
+        } else if (radioPaa.isChecked()) {
+            result = "おめでとう";
+        }
+        resultTextView.setText(result);
         Toast.makeText(this, "私はグー", Toast.LENGTH_SHORT).show();
     }
 }
